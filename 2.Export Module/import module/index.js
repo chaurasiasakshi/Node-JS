884b521343c142d4399799ee4f9e928ed3ab2068
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-01-27 17:41:03 +0530

    use require module

---
+++ 2.Export Module/import module/index.js
@@ -0,0 +1,6 @@
+import { square , PI} from "./math.js";
+// console.log(square(5,5));
+
+import {generate } from "random-words";
+
+console.log(generate())
---
+++ 2.Export Module/import module/math.js
@@ -0,0 +1,7 @@
+
+export const sum = (x , y) => x + y;
+export const substraction = (x , y) => x - y;
+export const multiply = (x , y) => x * y;
+export const divide = (x , y) => x / y;
+export const square = (x ) => x*x;
+export const PI = 3.14;
