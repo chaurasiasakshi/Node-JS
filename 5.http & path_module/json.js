8342de9215507d200d76008c8126de79d1b22abe
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-01-28 17:07:41 +0530

    learn json parse

--- 5.http & path_module/json.js
+++ 5.http & path_module/json.js
@@ -1,8 +1,8 @@
-const data = {
-    name :"sakshi",
-    age: 20,
-    location:"lucknow"
-};
+// const data = {
+//     name :"sakshi",
+//     age: 20,
+//     location:"lucknow"
+// };
 // console.log(data)
 
 // const jsonData = JSON.stringify(data);
@@ -14,3 +14,27 @@ const data = {
 // { name: 'sak', age: 20, location: 'luck' }
 // JSON DATA {"name":"sak","age":20,"location":"luck"}
 
+const fs = require("fs");
+const data = {
+    name :"sakshi",
+    age: 20,
+    location:"lucknow"
+};
+
+// 1 . Accessing the property of an object using dot notation
+console.log(`Name is ${data.name}`);
+// 2 . Using bracket notation to access properties 
+console.log(`Age is ${data['age']}`);
+
+
+const jsonDATA = JSON.stringify(data);
+// fs.writeFile('json.json',jsonDATA,(err)=>{
+//     console.log("done");
+// });
+
+fs.readFile('json.json','utf-8',(err,data)=>{
+console.log(data); 
+const orgData = JSON.parse(data);
+console.log(orgData); 
+
+});
