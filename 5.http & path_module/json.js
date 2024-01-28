88403f00ad4a3af3f70af96dfb8a6144791f0b66
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-01-28 16:56:42 +0530

    JSON Format

---
+++ 5.http & path_module/json.js
@@ -0,0 +1,16 @@
+const data = {
+    name :"sakshi",
+    age: 20,
+    location:"lucknow"
+};
+// console.log(data)
+
+// const jsonData = JSON.stringify(data);
+// const objData = JSON.parse(jsonData);
+
+// console.log(objData);
+// console.log("JSON DATA",jsonData);
+
+// { name: 'sak', age: 20, location: 'luck' }
+// JSON DATA {"name":"sak","age":20,"location":"luck"}
+
