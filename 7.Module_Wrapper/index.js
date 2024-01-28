ddd4fea58bc9c4e926ec82ef1b1805a218be7dd4
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-01-28 16:34:10 +0530

    Wrapper Modulr

---
+++ 7.Module_Wrapper/index.js
@@ -0,0 +1,7 @@
+( function() {
+    const myname ="SAKSHI";
+    console.log(myname);
+})
+
+console.log("IIFE --> Immediately Invoked Function Expression");
+// function(export,require,module,__filename,__dirname)
